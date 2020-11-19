8d874dc900ac160acac28231938253572c75a0de
7798209972 <patlesachin1@gmail.com>
2020-11-19 09:21:35 +0530

    Welcome Message - Indian State Census Analyser Problem

---
+++ indian_state_census_analyser.php
@@ -0,0 +1,15 @@
+<?php
+class StateCensusAnalyser
+{
+    //Using constructor for welcome message
+    function __construct()
+    {
+        echo "=========================================================\n";
+        echo "Welcome to Indian Census Analyser Problem \n";
+        echo "=========================================================\n";
+    }
+}
+
+$state_cencus_analyser_class_object= new StateCensusAnalyser();
+
+?>
