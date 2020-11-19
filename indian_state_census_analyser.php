757655ef9cadbcfee8e5089b8deeb53e250ae87d
7798209972 <patlesachin1@gmail.com>
2020-11-19 09:47:52 +0530

    Created classes to load Indian State Cencus Information from a CSV file

--- indian_state_census_analyser.php
+++ indian_state_census_analyser.php
@@ -8,8 +8,51 @@ class StateCensusAnalyser
         echo "Welcome to Indian Census Analyser Problem \n";
         echo "=========================================================\n";
     }
-}
 
-$state_cencus_analyser_class_object= new StateCensusAnalyser();
+    //Method to load State Census CSV data
+    function load_state_census_csv_data()
+    {
+        try
+        {
+            //Checking file exists or not
+            if(!file_exists("StateCensusData.csv"))
+            {
+                throw new Exception("StateCensusData.csv not found");       //Throw excedption
+            }
+            else
+            {
+                $file = fopen("StateCensusData.csv", "r");      //Opening CSV file
+                $chunk_size = 1024*1024;                        //Size(1MB) to chunk file 
+                while (!feof($file))
+                {
+                    //Getting file data according to chunk size
+                    $data = fgetcsv($file, $chunk_size);
+                    foreach($data as $arr)
+                    {
+                        echo $arr."\n";
+                    }
+                }
+                fclose($file);      //Closing File
+            }
+        }
+        catch(Exception $err)
+        {
+            //Getting error message into a variable
+            $error_message=$err->getMessage();
+        }
+    }
+}
+class CSVStateCensus
+{
+    //Method to load CSV data
+    function load_csv_data()
+    {
+        $state_cencus_analyser_class_object= new StateCensusAnalyser();     //Creating object of StateCensusAnalyser
+        $state_cencus_analyser_class_object->load_state_census_csv_data();      //Calling method of StateCensusAnalyser
+    }
+}
+//Object Declaration
+$csv_state_census=new CSVStateCensus();     //Creating object of CSVStateCensus
+$csv_state_census->load_csv_data();     //Calling method CSVStateCensus
 
 ?>
